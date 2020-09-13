106064028e11678db2355a8eb14e7caccc512984
Stephen Davis <davi1138@purdue.edu>
2020-09-12 20:22:53 -0400

    Added boilerplate React

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { LoadAssets } from './components'
+
+const fonts = {
+  "SFProDisplay-Bold": require('./assets/fonts/SF-Pro-Display-Bold.otf')
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LoadAssets {... {fonts}} >
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </LoadAssets>
   );
 }
 
