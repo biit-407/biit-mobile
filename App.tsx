16cb6e95462734568f3645aa1e1a72564e5e1e4a
Daniel Kambich <dkambich@purdue.edu>
2020-09-16 21:09:49 -0400

    Add basic navigation

--- App.tsx
+++ App.tsx
@@ -1,29 +1,23 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 import { LoadAssets } from "./src/components";
+import LoginPage from "./src/components/authentication/LoginPage";
 
 const fonts = {
   "SFProDisplay-Bold": require("./assets/fonts/SF-Pro-Display-Bold.otf"),
 };
 
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
     <LoadAssets {...{ fonts }}>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={LoginPage} />
+      </Stack.Navigator>
     </LoadAssets>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
