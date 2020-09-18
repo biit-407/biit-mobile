4cd249852c9667d8196fa8bacdb20181c818d62e
Stephen Davis <davi1138@purdue.edu>
2020-09-18 11:51:07 -0400

    Updated hook exports

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,1 +1,3 @@
 export { default as useAzureAuth } from "./useAzureAuth";
+export { default as useAzureToken } from "./useAzureToken";
+export { default as useAzureUserInfo } from "./useAzureUserInfo";
