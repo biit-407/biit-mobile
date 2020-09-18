598d35be4c026403082493de246b28cf4e6b24a4
Stephen Davis <davi1138@purdue.edu>
2020-09-18 11:37:16 -0400

    Added constants file

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+/**
+ * @description ID of the resource group in Azure (purdue.edu)
+ */
+export const AZURE_TENANT_ID = "4130bd39-7c53-419c-b1e5-8758d6d63f21";
+
+/**
+ * @description ID of the client application accessing the 
+ * tenant resources (biit)
+ */
+export const AZURE_CLIENT_ID = "c128fe76-dc54-4daa-993c-1a13c1e82080";
