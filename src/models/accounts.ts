06c500227536a282d0181ee71e5d9ed2fe72fd31
Stephen Davis <davi1138@purdue.edu>
2020-09-22 09:51:54 -0400

    Created accounts post model

---
+++ src/models/accounts.ts
@@ -0,0 +1,16 @@
+/**
+ * @description model used to send data to the Account
+ * POST endpoint. for more information see the documentation
+ * here https://github.com/biit-407/biit-server/wiki/Endpoints#account
+ * 
+ * @field fname: the first name of the user on the account
+ * @field lname: the last name of the user on the account
+ * @field email: the email of the user on the account 
+ * 
+ * *NOTE*: Currently all of these fields should be derived from Azure
+ */
+export interface AccountPOST {
+    fname: string;
+    lname: string;
+    email: string;
+}
