5f85c14a08fd02876a5defcd8b5debdf9c59b7d1
Stephen Davis <davi1138@purdue.edu>
2020-09-29 20:39:49 -0400

    Added server url constant

--- src/models/constants.ts
+++ src/models/constants.ts
@@ -8,3 +8,8 @@ export const AZURE_TENANT_ID = "4130bd39-7c53-419c-b1e5-8758d6d63f21";
  * tenant resources (biit)
  */
 export const AZURE_CLIENT_ID = "c128fe76-dc54-4daa-993c-1a13c1e82080";
+
+/**
+ * @description URL of the currently deployed server instance
+ */
+export const SERVER_ADDRESS = "our-server-address-url-goes-here"
