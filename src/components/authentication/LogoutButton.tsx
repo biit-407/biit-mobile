0c6a4ff4e4ad403cdce09bfc422a1ac78f332fba
Daniel Kambich <dkambich@purdue.edu>
2020-10-06 16:06:31 -0400

    Integrate LogoutButton

--- src/components/authentication/LogoutButton.tsx
+++ src/components/authentication/LogoutButton.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Alert } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 import ThemedButton from "../themed/ThemedButton";
+import { useAccount } from "../../contexts/accountContext";
 
 export default function LogoutButton() {
   const navigation = useNavigation();
+  const [accountState, accountDispatch] = useAccount();
+
+  // Listen for account state to be logged out
+  useEffect(() => {
+    if (accountState.status === "logged out") {
+      // Navigate back to the login page once account is logged out
+      navigation.reset({
+        index: 0,
+        routes: [{ name: "Login" }],
+      });
+    }
+  }, [accountState.status, navigation]);
+
   const showLogoutDialog = () => {
     Alert.alert("Logout?", "Are you sure you want to logout of your account?", [
       {
@@ -15,12 +29,8 @@ export default function LogoutButton() {
       {
         text: "Confirm",
         onPress: () => {
-          // TODO: Run the logout hook
-          //Navigate back to login
-          navigation.reset({
-            index: 0,
-            routes: [{ name: "Login" }],
-          });
+          // Invalidate the current user account to log them out
+          accountDispatch({ type: "invalidate", ...accountState });
         },
       },
     ]);
