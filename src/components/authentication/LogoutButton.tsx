e100aae00afa10bf3093538d18513be4f51955e5
Daniel Kambich <31736354+DanielKambich@users.noreply.github.com>
2020-10-06 16:29:12 -0400

    Integrate LogoutButton (#17)
    * Integrate LogoutButton
    
    * Add token and azure clearing in LogoutButton

--- src/components/authentication/LogoutButton.tsx
+++ src/components/authentication/LogoutButton.tsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Alert } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 import ThemedButton from "../themed/ThemedButton";
+import { useAccount } from "../../contexts/accountContext";
+import { useToken } from "../../contexts/tokenContext";
+import { useAzure } from "../../contexts/azureContext";
+import { BLANK_AZURE_USER_INFO, BLANK_TOKEN } from "../../models/azure";
 
 export default function LogoutButton() {
   const navigation = useNavigation();
+  const [accountState, accountDispatch] = useAccount();
+  const [, tokenDispatch] = useToken();
+  const [, azureDispatch] = useAzure();
+
+  // Listen for account state to be logged out
+  useEffect(() => {
+    if (accountState.status === "logged out") {
+      // Navigate back to the login page once account is logged out
+      navigation.reset({
+        index: 0,
+        routes: [{ name: "Login" }],
+      });
+    }
+  }, [accountState.status, navigation]);
+
   const showLogoutDialog = () => {
     Alert.alert("Logout?", "Are you sure you want to logout of your account?", [
       {
@@ -15,11 +34,14 @@ export default function LogoutButton() {
       {
         text: "Confirm",
         onPress: () => {
-          // TODO: Run the logout hook
-          //Navigate back to login
-          navigation.reset({
-            index: 0,
-            routes: [{ name: "Login" }],
+          // Invalidate the current user account to log them out
+          accountDispatch({ type: "invalidate", ...accountState });
+          tokenDispatch({ ...BLANK_TOKEN, type: "clear" });
+          azureDispatch({
+            type: "invalidate",
+            ...BLANK_TOKEN,
+            grantToken: "",
+            userInfo: BLANK_AZURE_USER_INFO,
           });
         },
       },
