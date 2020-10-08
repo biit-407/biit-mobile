eb9792cbdde5b9ce94ad7b3c3e58de797b2c965c
Daniel Kambich <31736354+DanielKambich@users.noreply.github.com>
2020-10-08 17:25:50 -0400

    Ban user integration (#33)
    * Add integrate ban user
    
    * Fix linting

--- src/components/communities/MemberListPage.tsx
+++ src/components/communities/MemberListPage.tsx
@@ -1,18 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, SectionList } from "react-native";
+import { StyleSheet, SectionList, Alert } from "react-native";
 
 import {
+  banUserFromCommunity,
   getCommunity,
   loadCommunity,
   useCommunity,
 } from "../../contexts/communityContext";
 import { useToken } from "../../contexts/tokenContext";
+import { Account } from "../../models/accounts";
+import { BLANK_COMMUNITY, Community } from "../../models/community";
+import { useAccountState } from "../../contexts/accountContext";
 import {
   MemberListPageRouteProp,
   MemberListPageNavigationProp,
 } from "../../routes";
 import Box from "../themed/Box";
 import Text from "../themed/Text";
+import ThemedIcon from "../themed/ThemedIcon";
+import ThemedListItem from "../themed/ThemedListItem";
 
 type MemberListPageProps = {
   route: MemberListPageRouteProp;
@@ -35,25 +41,23 @@ const styles = StyleSheet.create({
   list: {
     width: "100%",
   },
-  listitem: {
-    paddingLeft: 10,
-    borderBottomWidth: 1,
-  },
   header: {
     paddingLeft: 5,
   },
 });
 
-const Item = ({ title }: { title: string }) => (
-  <Box style={styles.listitem}>
-    <Text>{title}</Text>
-  </Box>
-);
+// const Item = ({ title }: { title: string }) => (
+//   <Box style={styles.listitem}>
+//     <Text>{title}</Text>
+//   </Box>
+// );
 
 export default function MemberListPage({ route }: MemberListPageProps) {
   const [communityState, communityDispatch] = useCommunity();
   const [data, setData] = useState<{ title: string; data: string[] }[]>([]);
+  const [community, setCommunity] = useState<Community>(BLANK_COMMUNITY);
   const [tokenState, tokenDispatch] = useToken();
+  const accountState = useAccountState();
 
   useEffect(() => {
     // automatically queue a data update
@@ -66,17 +70,69 @@ export default function MemberListPage({ route }: MemberListPageProps) {
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   useEffect(() => {
-    const community = getCommunity(communityState, route.params.name);
+    const tempCommunity = getCommunity(communityState, route.params.name);
     setData([
-      { title: "Admins", data: community.Admins },
-      { title: "Members", data: community.Members },
+      { title: "Admins", data: tempCommunity.Admins },
+      { title: "Members", data: tempCommunity.Members },
     ]);
+    setCommunity(tempCommunity);
   }, [communityState, route.params.name]);
+
+  const promptBanUser = (email: string, communityName: string) => {
+    Alert.alert(
+      "Ban " + email,
+      "Are you sure you want to ban " + email + " from " + communityName + "?",
+      [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        {
+          text: "OK",
+          onPress: () => {
+            banUserFromCommunity(
+              tokenDispatch,
+              tokenState.refreshToken,
+              accountState.account.email,
+              email,
+              communityName
+            );
+          },
+        },
+      ]
+    );
+  };
+
+  const renderBanHammer = (
+    account: Account,
+    communityInfo: Community,
+    memberEmail: string
+  ) => {
+    const admins = communityInfo.Admins;
+    if (!admins.includes(account.email) || admins.includes(memberEmail)) {
+      return null;
+    }
+    return (
+      <ThemedIcon
+        size={24}
+        name="cross"
+        type="entypo"
+        onPress={() => promptBanUser(memberEmail, communityInfo.name)}
+      />
+    );
+  };
+
+  const Item = ({ title }: { title: string }) => (
+    <ThemedListItem
+      title={title}
+      rightContent={renderBanHammer(accountState.account, community, title)}
+    />
+  );
 
   return (
     <Box style={styles.root}>
-      <Box>
-        <Text>Community Name</Text>
+      <Box marginVertical="md">
+        <Text variant="header">{community.name}</Text>
       </Box>
       <SectionList
         style={styles.list}
