7e576a7661525ee26c3dd9ffda0258e0a03f397f
Daniel Kambich <dkambich@purdue.edu>
2020-11-24 15:20:36 -0500

    Clean and comment PreviousUsersPage

--- src/components/meetups/PreviousUsersPage.tsx
+++ src/components/meetups/PreviousUsersPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FlatList, ListRenderItemInfo, StyleSheet } from "react-native";
 
 import {
@@ -7,7 +7,7 @@ import {
 } from "../../routes";
 import Box from "../themed/Box";
 import useConstructor from "../../hooks/useConstructor";
-import { ThemedListItem } from "../themed";
+import { Text, ThemedListItem } from "../themed";
 import { PreviousUser } from "../../models/accounts";
 import { EMPTY_PROFILE_PIC } from "../../models/constants";
 import { BLANK_MEETUP } from "../../models/meetups";
@@ -27,47 +27,57 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     justifyContent: "flex-start",
   },
+  list: {
+    flexGrow: 1,
+  },
 });
 
 export default function PreviousUsersPage({
   navigation,
 }: PreviousUsersPageProps) {
-  //   const [meetups, setMeetups] = React.useState<Meetup[]>([]);
-  //   const load = (meetupsList: Meetup[]) => {
-  //     setMeetups(meetupsList);
-  //   };
+  // Create state for the list of previous users
+  const [previousUsers, setPreviousUsers] = useState<PreviousUser[]>();
 
-  const previousUsers: PreviousUser[] = [
-    {
-      fname: "Daniel",
-      lname: "Kambich",
-      email: "test@gmail.com",
-      commonMeetups: [
-        { ...BLANK_MEETUP },
-        { ...BLANK_MEETUP },
-        { ...BLANK_MEETUP },
-      ],
-    },
-  ];
+  // Load in previous users
   useConstructor(() => {
-    // TODO: Load in previous people objects
-    // load(pastMeetups);
+    // TODO: Load in previous users
+    const loadedUsers = [
+      {
+        fname: "Daniel",
+        lname: "Kambich",
+        email: "test@gmail.com",
+        commonMeetups: [{ ...BLANK_MEETUP }],
+      },
+    ];
+    // Set the previous users
+    setPreviousUsers(loadedUsers);
   });
 
+  // Component to be rendered for each person
   const renderPerson = ({ item }: ListRenderItemInfo<PreviousUser>) => (
     <ThemedListItem
       avatarUri={item.profileImage ?? EMPTY_PROFILE_PIC}
       title={`${item.fname} ${item.lname}`}
       onPress={() => navigation.push("PreviousProfile", { previousUser: item })}
+      chevron
     />
   );
 
+  // Component to be rendered if list is empty
+  const emptyList = () => (
+    <Box flex={1} justifyContent="center" alignItems="center">
+      <Text variant="listHeader">You have not met with any users</Text>
+    </Box>
+  );
+
   return (
-    <Box backgroundColor="mainBackground" style={{ ...styles.root }}>
+    <Box backgroundColor="mainBackground" style={styles.root}>
       <FlatList
         data={previousUsers}
         keyExtractor={(user) => user.email}
         renderItem={renderPerson}
+        ListEmptyComponent={emptyList}
+        contentContainerStyle={styles.list}
       />
     </Box>
   );
