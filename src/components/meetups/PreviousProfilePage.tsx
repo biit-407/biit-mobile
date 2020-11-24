8b9994e4e72a104e8774484e89deaf28768a882c
Daniel Kambich <dkambich@purdue.edu>
2020-11-24 15:26:06 -0500

    Clean and comment PreviousProfilePage

--- src/components/meetups/PreviousProfilePage.tsx
+++ src/components/meetups/PreviousProfilePage.tsx
@@ -6,7 +6,7 @@ import {
   PreviousProfilePageRouteProp,
   PreviousProfilePageNavigationProp,
 } from "../../routes";
-import { ProfileCard, ThemedButton } from "../themed";
+import { ProfileCard, Text, ThemedButton } from "../themed";
 import Box from "../themed/Box";
 
 import MeetupCard from "./MeetupCard";
@@ -24,7 +24,6 @@ const styles = StyleSheet.create({
   root: {
     flex: 1,
     flexDirection: "column",
-    alignItems: "center",
   },
 });
 
@@ -35,21 +34,36 @@ export default function PreviousProfilePage({
     previousUser: { fname, lname, commonMeetups },
   } = route.params;
 
+  // TODO: Integrated reconnect logic
   const reconnect = () => true;
 
+  const emptyList = () => (
+    <Text textAlign="center" variant="body">
+      You have not had any meetups with {fname}
+    </Text>
+  );
+
   return (
-    <Box backgroundColor="mainBackground" style={{ ...styles.root }}>
-      <Box width="100%">
-        <ProfileCard name={`${fname} ${lname}`} />
-        <ThemedButton
-          title={`Reconnect with ${fname} ${lname}`}
-          onPress={reconnect}
-        />
-        <FlatList
-          data={commonMeetups}
-          renderItem={({ item }) => <MeetupCard {...item} />}
-        />
-      </Box>
+    <Box backgroundColor="mainBackground" style={styles.root}>
+      <FlatList
+        data={commonMeetups}
+        keyExtractor={(meetup) => meetup.id}
+        renderItem={({ item }) => <MeetupCard {...item} isClickable={false} />}
+        ListHeaderComponent={
+          <>
+            <ProfileCard name={`${fname} ${lname}`} />
+            <ThemedButton
+              title={`Reconnect with ${fname}`}
+              onPress={reconnect}
+            />
+            <Text textAlign="center" variant="subheader">
+              Your previous meetups with {fname}
+            </Text>
+          </>
+        }
+        ListFooterComponent={<Box marginVertical="lg" />}
+        ListEmptyComponent={emptyList}
+      />
     </Box>
   );
 }
