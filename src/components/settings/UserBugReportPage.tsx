c42391886e99476efad167da1f85880746bf4361
Daniel Kambich <dkambich@purdue.edu>
2020-11-27 23:57:47 -0600

    Update BugReport UI

--- src/components/settings/UserBugReportPage.tsx
+++ src/components/settings/UserBugReportPage.tsx
@@ -1,15 +1,18 @@
-import React, { useState } from "react";
-import { StyleSheet, TextInput } from "react-native";
-import { Picker } from "@react-native-community/picker";
-import { SubmitHandler } from "react-hook-form";
+import React, { useState } from 'react';
+import { SubmitHandler } from 'react-hook-form';
+import { StyleSheet, TextInput } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
-import Box from "../themed/Box";
-import Text from "../themed/Text";
-import ThemedButton from "../themed/ThemedButton";
-import theme from "../../theme";
-import { reportBug, useAccountState } from "../../contexts/accountContext";
-import { useToken } from "../../contexts/tokenContext";
-import { SettingsRoutes, StackNavigationProps } from "../../routes";
+import { Picker } from '@react-native-community/picker';
+
+import { reportBug, useAccountState } from '../../contexts/accountContext';
+import { useToken } from '../../contexts/tokenContext';
+import { SettingsRoutes, StackNavigationProps } from '../../routes';
+import theme from '../../theme';
+import { ThemedInput } from '../themed';
+import Box from '../themed/Box';
+import Text from '../themed/Text';
+import ThemedButton from '../themed/ThemedButton';
 
 export const UserBugReportPageOptions = {
   title: "Bug Report",
@@ -20,7 +23,6 @@ const styles = StyleSheet.create({
   root: {
     flex: 1,
     flexDirection: "column",
-    justifyContent: "space-between",
   },
   inputLabelBox: {
     borderTopWidth: 2,
@@ -101,83 +103,80 @@ export default function UserBugReportPage({
 
   return (
     <Box backgroundColor="mainBackground" style={styles.root}>
-      <Box style={styles.inputLabelBox}>
-        <Text style={styles.inputLabel}>
+      <ScrollView style={{ flexGrow: 1 }}>
+        <Text variant="subheader" textAlign="center" mt="md">
           Found a bug? Let us know about it!
         </Text>
-      </Box>
-      <Box style={{ justifyContent: "space-evenly" }}>
-        <Text style={styles.subtitle}>Where is the bug located?</Text>
-        <Picker
-          selectedValue={bugLocation}
-          style={{ height: 50, width: 300, alignSelf: "center" }}
-          onValueChange={(itemValue, _itemIndex) => {
-            setBugLocation(itemValue.toString());
-          }}
-        >
-          <Picker.Item label="Login Page" value="login" />
-          <Picker.Item label="Create Account Page" value="createAccount" />
-          <Picker.Item label="Create Profile Page" value="createProfile" />
-          <Picker.Item label="View Profile Page" value="viewProfile" />
-          <Picker.Item label="Edit Profile Page" value="editProfile" />
-          <Picker.Item label="Banned Users Page" value="bannedUsers" />
-          <Picker.Item label="Create Community Page" value="createCommunity" />
-          <Picker.Item label="Join Community" value="joinCommunity" />
-          <Picker.Item label="Leave Community" value="leaveCommunity" />
-          <Picker.Item label="Member List Page" value="memberList" />
-          <Picker.Item label="Code of Conduct Page" value="codeOfConduct" />
-          <Picker.Item
-            label="Community Administration Page"
-            value="communityAdministration"
+        <Box m="sm">
+          <Text variant="body" textAlign="center">
+            Where is the bug located?
+          </Text>
+          <Picker
+            selectedValue={bugLocation}
+            style={{ height: 50, width: 300, alignSelf: "center" }}
+            onValueChange={(itemValue, _itemIndex) => {
+              setBugLocation(itemValue.toString());
+            }}
+          >
+            <Picker.Item label="Login Page" value="login" />
+            <Picker.Item label="Create Account Page" value="createAccount" />
+            <Picker.Item label="Create Profile Page" value="createProfile" />
+            <Picker.Item label="View Profile Page" value="viewProfile" />
+            <Picker.Item label="Edit Profile Page" value="editProfile" />
+            <Picker.Item label="Banned Users Page" value="bannedUsers" />
+            <Picker.Item
+              label="Create Community Page"
+              value="createCommunity"
+            />
+            <Picker.Item label="Join Community" value="joinCommunity" />
+            <Picker.Item label="Leave Community" value="leaveCommunity" />
+            <Picker.Item label="Member List Page" value="memberList" />
+            <Picker.Item label="Code of Conduct Page" value="codeOfConduct" />
+            <Picker.Item
+              label="Community Administration Page"
+              value="communityAdministration"
+            />
+            <Picker.Item label="Settings Page" value="userSettings" />
+            <Picker.Item label="Meetup List" value="meetupList" />
+            <Picker.Item label="Meetup Details Page" value="meetupDetails" />
+            <Picker.Item label="Meetup Response Page" value="meetupResponse" />
+            <Picker.Item label="Rating Meetups" value="meetupRating" />
+            <Picker.Item label="Location Ranking Page" value="locationRanker" />
+            <Picker.Item
+              label="Previous Meetups Page"
+              value="previousMeetups"
+            />
+            <Picker.Item label="Time Preference" value="userTimePreference" />
+            <Picker.Item label="Home Page" value="home" />
+            <Picker.Item label="Community Home Page" value="communityHome" />
+            <Picker.Item label="Feedback Page" value="feedback" />
+            <Picker.Item label="Bug Report Page" value="bugReport" />
+          </Picker>
+        </Box>
+        <Text variant="body" textAlign="center">
+          Please briefly describe the issue:
+        </Text>
+        <Box m="md">
+          <ThemedInput
+            multiline
+            onChangeText={(text) => setBugText(text)}
+            placeholder="Describe the issue..."
+            placeholderTextColor={theme.colors.primaryText}
+            value={bugText}
           />
-          <Picker.Item label="Settings Page" value="userSettings" />
-          <Picker.Item label="Meetup List" value="meetupList" />
-          <Picker.Item label="Meetup Details Page" value="meetupDetails" />
-          <Picker.Item label="Meetup Response Page" value="meetupResponse" />
-          <Picker.Item label="Rating Meetups" value="meetupRating" />
-          <Picker.Item label="Location Ranking Page" value="locationRanker" />
-          <Picker.Item label="Previous Meetups Page" value="previousMeetups" />
-          <Picker.Item label="Time Preference" value="userTimePreference" />
-          <Picker.Item label="Home Page" value="home" />
-          <Picker.Item label="Community Home Page" value="communityHome" />
-          <Picker.Item label="Feedback Page" value="feedback" />
-          <Picker.Item label="Bug Report Page" value="bugReport" />
-        </Picker>
-      </Box>
-      <Box>
-        <Text style={styles.subtitle}>Describe the issue:</Text>
-      </Box>
-      <Box style={styles.textInputBound}>
-        <TextInput
-          style={styles.textInput}
-          textAlignVertical="top"
-          multiline
-          onChangeText={(text) => setBugText(text)}
-          placeholder="Describe the issue..."
-          placeholderTextColor={theme.colors.primaryText}
-          value={bugText}
-        />
-      </Box>
-      <Box style={styles.buttonsBox}>
-        <ThemedButton
-          title="Cancel"
-          onPress={() => {
-            navigation.goBack();
-          }}
-          color="buttonDanger"
-        />
-        <ThemedButton
-          color="buttonSecondaryBackground"
-          title="Submit"
-          onPress={() => {
-            const bugReport: BugFormValues = {
-              location: bugLocation,
-              description: bugText,
-            };
-            submitBugReport(bugReport);
-          }}
-        />
-      </Box>
+        </Box>
+      </ScrollView>
+
+      <ThemedButton
+        title="Submit Report"
+        onPress={() => {
+          const bugReport: BugFormValues = {
+            location: bugLocation,
+            description: bugText,
+          };
+          submitBugReport(bugReport);
+        }}
+      />
     </Box>
   );
 }
