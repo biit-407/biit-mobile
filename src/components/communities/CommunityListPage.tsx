ffe0453f9c0957f7b7cef3ab832c63aed18156a1
Daniel Kambich <dkambich@purdue.edu>
2020-12-02 16:43:28 -0500

    Add integration for CommunityList

--- src/components/communities/CommunityListPage.tsx
+++ src/components/communities/CommunityListPage.tsx
@@ -1,12 +1,20 @@
-import React, { useState } from 'react';
-import { StyleSheet } from 'react-native';
-import { FlatList } from 'react-native-gesture-handler';
+import React, { useEffect, useState } from "react";
+import { StyleSheet } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 
-import { BLANK_COMMUNITY, Community } from '../../models/community';
-import { CommunityRoutes, StackNavigationProps } from '../../routes';
-import { Text, ThemedIcon, ThemedListItem } from '../themed';
-import Box from '../themed/Box';
-import ThemedSearchBar from '../themed/ThemedSearchBar';
+import { useAccountState } from "../../contexts/accountContext";
+import { getAllCommunities } from "../../contexts/communityContext";
+import { useToken } from "../../contexts/tokenContext";
+import { Community } from "../../models/community";
+import { CommunityRoutes, StackNavigationProps } from "../../routes";
+import {
+  Text,
+  ThemedIcon,
+  ThemedListItem,
+  ThemedRefreshControl,
+} from "../themed";
+import Box from "../themed/Box";
+import ThemedSearchBar from "../themed/ThemedSearchBar";
 
 export const CommunityListPageOptions = {
   tabBarIcon: ({
@@ -48,17 +56,21 @@ const styles = StyleSheet.create({
 export default function CommunityListPage({
   navigation,
 }: StackNavigationProps<CommunityRoutes, "CommunityList">) {
-  // TODO: Actually use community state once the endpoint is created
-  // const [communityState, communityDispatch] = useCommunity();
+  // Retrive context variables
+  const {
+    account: { email },
+  } = useAccountState();
+  const [tokenState, tokenDispatch] = useToken();
 
-  const communities: Community[] = [
-    { ...BLANK_COMMUNITY, name: "biit" },
-    { ...BLANK_COMMUNITY, name: "Johnsons" },
-  ];
+  // Create state for loading in communities
+  const [refreshing, setRefreshing] = useState(false);
+  const [communities, setCommunities] = useState<Community[]>([]);
 
+  // Create state for filtering in communities
   const [filteredCommunities, setFilteredCommunities] = useState(communities);
-
   const [search, setSearch] = useState("");
+
+  // Function to filter communites based on the searchText
   const onSearch = (searchText: string) => {
     setSearch(searchText);
     searchText = searchText.toLowerCase().replace(/\s/g, "");
@@ -69,6 +81,35 @@ export default function CommunityListPage({
     setFilteredCommunities(filtered);
   };
 
+  // Function to load all the user's communities
+  const loadCommunities = async () => {
+    setRefreshing(true);
+    const myCommunities = await getAllCommunities(
+      email,
+      tokenState.refreshToken,
+      tokenDispatch
+    );
+    setCommunities(myCommunities ?? []);
+    setRefreshing(false);
+  };
+
+  // Effect to properly display results after loading in communities
+  useEffect(
+    () => onSearch(search),
+    [communities] // eslint-disable-line react-hooks/exhaustive-deps
+  );
+
+  // Allows automatic loading on the page when navigating to the page from a nested or parent screen
+  // Used in this case for when the user creates a new community and are redirected here
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", () => {
+      loadCommunities();
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [navigation]); // eslint-disable-line react-hooks/exhaustive-deps
+
   const renderListItem = ({ item }: { item: Community }) => (
     <ThemedListItem
       title={item.name}
@@ -78,6 +119,18 @@ export default function CommunityListPage({
       chevron
     />
   );
+
+  const listEmpty = () => {
+    const message =
+      communities.length === 0
+        ? "You are not part of any communities. Please join or create one!"
+        : "No communities matched your search";
+    return (
+      <Text textAlign="center" m="md" variant="subheader">
+        {message}
+      </Text>
+    );
+  };
 
   return (
     <Box style={styles.root} backgroundColor="mainBackground">
@@ -90,6 +143,13 @@ export default function CommunityListPage({
         data={filteredCommunities}
         renderItem={renderListItem}
         keyExtractor={(community) => community.name}
+        ListEmptyComponent={listEmpty}
+        refreshControl={
+          <ThemedRefreshControl
+            refreshing={refreshing}
+            onRefresh={loadCommunities}
+          />
+        }
       />
     </Box>
   );
